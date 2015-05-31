b2c2e53a68da5a04f88198e02b6ef7e81102598f
Jose Talens <jltalens@hotmail.com>
2015-05-31 16:52:55 +0100

    IDEA configured for TDD

---
+++ test/sources/org/jltalens/setup/test/HookupTest.java
@@ -0,0 +1,12 @@
+package org.jltalens.setup.test;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HookupTest {
+    @Test
+    public void isThisThingOn() throws Exception {
+
+    }
+}
