508b56965432aa8852038e31d9bf974665ac83f6
Jose Talens <jltalens@hotmail.com>
2015-05-31 17:36:28 +0100

    Added nonnegative fractions tests.

--- test/sources/org/jltalens/Math/FractionTests.java
+++ test/sources/org/jltalens/Math/FractionTests.java
@@ -19,6 +19,11 @@ public class FractionTests {
     @Test
     public void non_zero_plus_non_zero() throws Exception {
         assertEquals(4, new Fraction(1).add(new Fraction(3)).intValue());
+    }
 
+    @Test
+    public void negative_numbers() throws Exception {
+        assertEquals(-1, new Fraction(-1).add(new Fraction(0)).intValue());
+        assertEquals(2, new Fraction(3).add(new Fraction(-1)).intValue());
     }
 }
