6c3ec234a8050ae41a1303366ecaed53d54ca073
Jose Talens <jltalens@hotmail.com>
2015-06-06 14:12:24 +0100

    Added more tests cases for equals

--- test/sources/org/jltalens/Math/FractionEqualsTest.java
+++ test/sources/org/jltalens/Math/FractionEqualsTest.java
@@ -4,11 +4,36 @@ package org.jltalens.Math;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class FractionEqualsTest {
     @Test
     public void same_numerator_and_denominator() throws Exception {
         assertEquals(new Fraction(3,5), new Fraction(3,5));
+
+    }
+
+    @Test
+    public void different_numerators() throws Exception {
+        assertNotEquals(new Fraction(1,3), new Fraction(4,5));
+
+    }
+
+    @Test
+    public void different_denominators() throws Exception {
+        assertNotEquals(new Fraction(1,4), new Fraction(1,3));
+
+    }
+
+    @Test
+    public void whole_numbers_same_fractions() throws Exception {
+        assertEquals(new Fraction(4), new Fraction(4,1));
+
+    }
+
+    @Test
+    public void whole_numbers_not_equals_each_other() throws Exception {
+        assertNotEquals(new Fraction(3), new Fraction(2));
 
     }
 }
